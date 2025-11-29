f4bc9a250ced3dc47366455e7eabbb439748dd1e
ChandraBose-11 <chandrubose46@gmail.com>
2025-11-29 12:25:49 +0530

    updated

--- Server/Controllers/userController.js
+++ Server/Controllers/userController.js
@@ -337,12 +337,7 @@ export const deleteUser = async (req, res, next) => {
 export const signout = (req, res, next) => {
   try {
     res
-      .clearCookie("access_token",{
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        path: "/",
-      })
+      .clearCookie("access_token")
       .status(200)
       .json("User has been signed out");
   } catch (error) {
