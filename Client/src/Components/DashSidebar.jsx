be67e1c90e9e9a450cfa6c211558146700eb8a51
ChandraBose-11 <chandrubose46@gmail.com>
2025-11-29 14:51:14 +0530

    a

--- Client/src/Components/DashSidebar.jsx
+++ Client/src/Components/DashSidebar.jsx
@@ -46,7 +46,7 @@ export default function DashSidebar() {
   };
   return (
     <div>
-      <Sidebar className="w-full md:w-56">
+      <Sidebar className="w-full md:w-full">
         {/* <SidebarItem> */}
         <SidebarItemGroup className="flex flex-col gap-1">
           {currentUser && currentUser.isAdmin && (
