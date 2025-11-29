2965a3eeea02cc1f83d578dddc005a4ade37137c
ChandraBose-11 <chandrubose46@gmail.com>
2025-11-29 14:47:29 +0530

    w-full updated

--- Client/src/Components/DashPosts.jsx
+++ Client/src/Components/DashPosts.jsx
@@ -91,10 +91,10 @@ export default function DashPosts() {
   };
 
   return (
-    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+    <div className=" table-auto w-full overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
-          <Table hoverable={true} className="shadow-md">
+          <Table hoverable={true} className="shadow-md ">
             <TableHead>
               <TableRow>
                 <TableHeadCell>Date updated</TableHeadCell>
